0cf90231248bb0a5d83baedd94b7bd605987790d
Himal Rana <ranahimalyo@gmail.com>
2025-02-15 15:50:21 +0530

    Some Changes

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,10 +1,10 @@
 import { Card, Col, Row, Container } from 'react-bootstrap';
-import weddingImg from './logo.png';
-import receptionImg from './logo.png';
-import sangeetImg from './logo.png';
-import haldiImg from './mehandi.png';
-import furnitureImg from './logo.png';
-import seatingImg from './logo.png';
+// import weddingImg from './logo.png';
+// import receptionImg from './logo.png';
+// import sangeetImg from './logo.png';
+// import haldiImg from './mehandi.png';
+// import furnitureImg from './logo.png';
+// import seatingImg from './logo.png';
 import './Services.css';
 
 const Services = () => {
@@ -16,25 +16,27 @@ const Services = () => {
         <Row className="justify-content-center mb-4">
           <Col md={6}>
             <Card className="service-card">
-              <div className="card-img-container">
+              {/* <div className="card-img-container">
                 <Card.Img variant="top" src={weddingImg} className="zoom-img full-img" />
-              </div>
+              </div> */}
               <Card.Body>
                 <Card.Title>Wedding Decoration</Card.Title>
                 <Card.Text className='textcard'>
                   Elegant and beautiful wedding decorations for your special day.
                 </Card.Text>
+                <Card.Title>Price : </Card.Title>
               </Card.Body>
             </Card>
           </Col>
           <Col md={6}>
             <Card className="service-card">
-              <div className="card-img-container">
+              {/* <div className="card-img-container">
                 <Card.Img variant="top" src={receptionImg} className="zoom-img full-img" />
-              </div>
+              </div> */}
               <Card.Body>
                 <Card.Title>Reception Setup</Card.Title>
                 <Card.Text className='textcard'>Professional setup for your reception, tailored to your theme.</Card.Text>
+                <Card.Title>Price : </Card.Title>
               </Card.Body>
             </Card>
           </Col>
@@ -43,23 +45,25 @@ const Services = () => {
         <Row className="justify-content-center mb-4">
           <Col md={6}>
             <Card className="service-card">
-              <div className="card-img-container">
+              {/* <div className="card-img-container">
                 <Card.Img variant="top" src={sangeetImg} className="zoom-img full-img" />
-              </div>
+              </div> */}
               <Card.Body>
                 <Card.Title>Sangeet Decoration</Card.Title>
                 <Card.Text className='textcard'>Colorful and festive decorations for your Sangeet ceremony.</Card.Text>
+                <Card.Title>Price : </Card.Title>
               </Card.Body>
             </Card>
           </Col>
           <Col md={6}>
             <Card className="service-card">
-              <div className="card-img-container">
+              {/* <div className="card-img-container">
                 <Card.Img variant="top" src={haldiImg} className="zoom-img full-img circular-img " />
-              </div>
+              </div> */}
               <Card.Body>
                 <Card.Title>Haldi Decoration</Card.Title>
                 <Card.Text className='textcard'>Traditional Haldi ceremony decor to add to the spirit of the occasion.</Card.Text>
+                <Card.Title>Price : </Card.Title>
               </Card.Body>
             </Card>
           </Col>
@@ -68,23 +72,25 @@ const Services = () => {
         <Row className="justify-content-center">
           <Col md={6}>
             <Card className="service-card">
-              <div className="card-img-container">
+              {/* <div className="card-img-container">
                 <Card.Img variant="top" src={furnitureImg} className="zoom-img full-img" />
-              </div>
+              </div> */}
               <Card.Body>
                 <Card.Title>Rental Furniture</Card.Title>
                 <Card.Text className='textcard'>Rent stylish furniture and accessories for all types of events.</Card.Text>
+                <Card.Title>Price : </Card.Title>
               </Card.Body>
             </Card>
           </Col>
           <Col md={6}>
             <Card className="service-card">
-              <div className="card-img-container">
+              {/* <div className="card-img-container">
                 <Card.Img variant="top" src={seatingImg} className="zoom-img full-img" />
-              </div>
+              </div> */}
               <Card.Body>
                 <Card.Title>Sitting Arrangements</Card.Title>
                 <Card.Text className='textcard'>Comfortable and beautiful seating arrangements for your guests.</Card.Text>
+                <Card.Title>Price : </Card.Title>
               </Card.Body>
             </Card>
           </Col>
